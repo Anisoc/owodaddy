65c18a4c9d59d95a2811f90f13dd8eed6f2b6e7f
Yoav Levi <13078659+koaie@users.noreply.github.com>
2022-02-15 16:33:50 +0000

    new uwufier

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import owo from "owoifyx";
+import Owo from "uwuifier";
 import Chance from "chance";
 import "dotenv/config";
 import { Client, Intents } from "discord.js";
@@ -6,6 +6,16 @@ import { Client, Intents } from "discord.js";
 const chance = new Chance();
 const client = new Client({
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+const owo = new Owo({
+  spaces: {
+    faces: 0.5,
+    actions: 0.075,
+    stutters: 0.1,
+  },
+  words: 1,
+  exclamations: 1,
 });
 
 const c = process.env.OWO_C || 5;
@@ -22,15 +32,25 @@ client.on("ready", () => {
   console.log(`Logged in as ${client?.user?.tag}, with chance ${c}!`);
 });
 
-client.on("message", (msg) => {
-  if (msg.author.id == id) return;
-  const p = chance.integer({ min: 0, max: 100 });
-  console.log(msg.content, p);
-  if (c >= p)
-    msg.reply({
-      content: owo(msg.content),
-      allowedMentions: { repliedUser: false },
-    });
-});
+client.on(
+  "message",
+  (msg: {
+    author: { id: string };
+    content: string;
+    reply: (arg0: {
+      content: string;
+      allowedMentions: { repliedUser: boolean };
+    }) => void;
+  }) => {
+    if (msg.author.id == id) return;
+    const p = chance.integer({ min: 0, max: 100 });
+    console.log(msg.content, p);
+    if (c >= p)
+      msg.reply({
+        content: owo.uwuifySentence(msg.content),
+        allowedMentions: { repliedUser: false },
+      });
+  }
+);
 
 client.login(token);
